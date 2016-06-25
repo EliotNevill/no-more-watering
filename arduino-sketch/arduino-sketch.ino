6bad858f2cc3ead15d2278c66011f60943352748
EliotNevill <eliotnevill@hotmail.com>
2016-06-25 12:07:20 +0100

    Added Libraries

--- arduino-sketch/arduino-sketch.ino
+++ arduino-sketch/arduino-sketch.ino
@@ -1,6 +1,15 @@
+#include <LiquidCrystal.h>
+
+#include <EEPROM.h>
+
+#include <TimeAlarms.h>
+
+#include <Time.h>
+#include <TimeLib.h>
+
 void setup() {
   // put your setup code here, to run once:
-
+ 
 }
 
 void loop() {
